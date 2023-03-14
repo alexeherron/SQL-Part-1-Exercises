f771dc3edc99aa7e5e12defefc5f1eb93e7b6359
alexherron <alexeherron@gmail.com>
2023-03-13 19:27:46 -0500

    Added changes to SQL exercises unfinished

---
+++ SQLQuery_1.sql
@@ -0,0 +1,9 @@
+-- SELECT TOP 1000 * FROM books
+-- SELECT COUNT(title) FROM books
+-- QUESTION 3: Count the number of books where the original_publication_year is earlier than 1800.
+-- SELECT COUNT(title) FROM books WHERE original_publication_year > 1800
+-- QUESTION 4: Create a query that displays distinct authors from the table.
+-- SELECT DISTINCT authors FROM books
+-- QUESTION 5: Create a query that displays a count of all the books that contain a language code for English.  This could be represented in the table as "eng" or "en-".
+SELECT COUNT(title) FROM books WHERE language_code = "eng" 
+--OR language_code="en-"
